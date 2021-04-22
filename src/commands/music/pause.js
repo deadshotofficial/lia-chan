1964d447a19e2f7429ae1a8c4a2452982dddc347
DeadShot <dvdranz120@gmail.com>
2021-04-22 10:52:06 +0530

    Create pause.js

---
+++ src/commands/music/pause.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'pause',
+	description: 'Pause command.',
+	cooldown: 5,
+	execute(message) {
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (serverQueue && serverQueue.playing) {
+			serverQueue.playing = false;
+			serverQueue.connection.dispatcher.pause();
+			return message.channel.send('⏸ Paused!');
+		}
+		return message.channel.send('There is nothing playing.');
+	}
+};
