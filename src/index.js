2f20f7ddeb0673a07db4427b40f46ec7c943924a
DeadShot <dvdranz120@gmail.com>
2021-04-22 11:21:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ require('dotenv').config();
 const { readdirSync } = require('fs');
 const { join } = require('path');
 const MusicClient = require('./struct/Client');
-const { Collection } = require('discord.js');
+const { MessageEmbed, Collection } = require("discord.js");
 const client = new MusicClient({ token: process.env.DISCORD_TOKEN, prefix: process.env.DISCORD_PREFIX });
+
+const db = require("quick.db")
+const ms = require('parse-ms')
+const fs = require('fs')
+
+client.snipes = new Map()
+client.on('messageDelete', function(message, channel){
+  
+  client.snipes.set(message.channel.id, {
+    content:message.content,
+    author:message.author.tag,
+    image:message.attachments.first() ? message.attachments.first().proxyURL : null
+  })
+  
+});
 
 const commandFiles = readdirSync(join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -11,11 +26,39 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-client.once('ready', () => console.log('Ready to Perform!'));
-client.on('message', message => {
-	if (!message.content.startsWith(client.config.prefix) || message.author.bot) return;
-	const args = message.content.slice(client.config.prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`)
+    client.user.setActivity(('s!help'), {type: 'LISTENING'})
+})
+
+
+client.on("message", async message => {
+   
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    let prefix = db.get(`prefix_${message.guild.id}`)
+    if(prefix === null) prefix = process.env.DISCORD_PREFIX;
+
+    if (!message.content.startsWith(prefix)) return;
+
+    if (message.content.match(new RegExp(`^<@!?${client.user.id}>`))) {
+        const embed = new MessageEmbed()
+        .setTitle('Thanks for Adding Me!')
+        .setThumbnail(client.user.displayAvatarURL())
+        .setDescription(`Hey There User, Below are the Default Configs for the Guild:`)
+        .addField('Guild Prefix:', [
+            `${prefix} | To Change: <prefix>setprefix <new_prefix>`
+        ])
+        .setFooter(`Use ${prefix}help to see commands list`)
+        .setTimestamp();
+        return message.channel.send(embed);
+    }
+        
+ 
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const commandName = args.shift().toLowerCase();
 	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 	if (!command) return;
 	if (command.guildOnly && message.channel.type !== 'text') return message.reply('I can\'t execute that command inside DMs!');
@@ -34,7 +77,7 @@ client.on('message', message => {
 		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 		if (now < expirationTime) {
 			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`Please Wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+			return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
 		}
 	}
 	timestamps.set(message.author.id, now);
@@ -44,8 +87,9 @@ client.on('message', message => {
 		command.execute(message, args);
 	} catch (error) {
 		console.error(error);
-		message.reply(':no_entry_sign: | There was an error trying to execute that command!');
+		message.reply('There was an error trying to execute that command!');
 	}
+
 });
 
-client.login(client.config.token);
+client.login(token);
